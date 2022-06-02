dcd0969f49990050e7187fe6af66ec08ec78ee09
Yuma <bird.torichan@gmail.com>
2022-06-03 00:49:00 +0900

    fix test時と実行時でimportパスが異なることに起因するバグを修正した

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 import os
 from dotenv import load_dotenv
-from . import member_database as md
-from . import scrum_master as sm
-from . import task_storage as ts
+from src.member_database import MemberDatabase
+from src.scrum_master import ScrumMaster
+from src.task_storage import FailedTaskStorage
 
 class AsanaAlart():
     def main(self):
-        member_database = md.MemberDatabase()
+        member_database = MemberDatabase()
         members = member_database.get_members()
-        task_storage = ts.FailedTaskStorage()
-        scrumMaster = sm.ScrumMaster(members, task_storage)
+        task_storage = FailedTaskStorage()
+        scrumMaster = ScrumMaster(members, task_storage)
         scrumMaster.start_check_progress_cycle(5)
 
 if __name__ == '__main__':
