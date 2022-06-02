9a349fa26c7a5400aacfbc31b948914f284089d6
Yuma <bird.torichan@gmail.com>
2022-06-03 02:44:48 +0900

    fix 古くなったテストの内容を修正した

--- tests/member_database_test.py
+++ tests/member_database_test.py
@@ -3,11 +3,11 @@ from src.member_database import MemberDatabase
 def test_メンバーの名前一覧を取得する():
     db = MemberDatabase()
     members = db.get_members()
-    assert members[0].name == 'Torikoshi'
-    assert members[1].name == 'Segawa'
+    assert members[1].name == '伊藤'
+    assert members[5].name == '田中'
 
 def test_メンバーのasanaプロジェクトID一覧を取得する():
     db = MemberDatabase()
     members = db.get_members()
     assert members[0].asana_id == 1202070137346385
-    assert members[1].asana_id == 1202070137346378
+    assert members[1].asana_id == 1202120929387877
